342f862f81a7e932e64af170543379470dd96438
joshQuirey <jquirey@icloud.com>
2016-02-07 10:30:09 -0600

    Begin process of adding text view to journal entry view

--- DarndestApp/ComposeVC.swift
+++ DarndestApp/ComposeVC.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ComposeVC: UIViewController {
+class ComposeVC: UIViewController, UITextViewDelegate {
 
+    var note : String = ""
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        createTextView()
+        //var textStorage: SyntaxHighlightTextStorage!
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +36,32 @@ class ComposeVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func createTextView() {
+        // 1. Create the text storage that backs the editor
+        let attrs = [NSFontAttributeName : UIFont.preferredFontForTextStyle(UIFontTextStyleBody)]
+        _ = NSAttributedString(string: note, attributes: attrs)
+        //xtStorage = SyntaxHighlightTextStorage()
+        //textStorage.appendAttributedString(attrString)
+        
+        let newTextViewRect = view.bounds
+        
+        // 2. Create the layout manager
+        let layoutManager = NSLayoutManager()
+        
+        // 3. Create a text container
+        let containerSize = CGSize(width: newTextViewRect.width, height: CGFloat.max)
+        let container = NSTextContainer(size: containerSize)
+        container.widthTracksTextView = true
+        layoutManager.addTextContainer(container)
+        //textStorage.addLayoutManager(layoutManager)
+        
+        // 4. Create a UITextView
+        textView = UITextView(frame: newTextViewRect, textContainer: container)
+        textView.delegate = self
+        view.addSubview(textView)
+    }
+
     
     @IBAction func cancelTapped(sender: AnyObject) {
         
